78f0082af6be0c549adb18c51ae4629b7fb5d754
manhcuong1502222 <acuongsh@gmail.com>
2024-05-20 22:49:43 +0700

    b

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,20 @@
 // Sensor pins
 #define TRIG_PIN_1 2
 #define ECHO_PIN_1 3
-
 #define TRIG_PIN_2 4
 #define ECHO_PIN_2 5
-
 #define TRIG_PIN_3 6
 #define ECHO_PIN_3 7
-
 #define TRIG_PIN_4 8
 #define ECHO_PIN_4 9
 
-// LED pins for each sensor (2 LEDs per sensor, using analog pins A0-A7)
+// LED pins for each sensor 
 #define LED_RED_1 A0
 #define LED_GREEN_1 A1
-
 #define LED_RED_2 A2
 #define LED_GREEN_2 A3
-
 #define LED_RED_3 A4
 #define LED_GREEN_3 A5
-
 #define LED_RED_4 A6
 #define LED_GREEN_4 A7
 
@@ -78,14 +72,17 @@ void loop() {
   float distance4 = getDistance(TRIG_PIN_4, ECHO_PIN_4);
 
   int ledCount = 0;
+  int ledStatus[4];
 
   // Sensor 1 logic
   if (distance1 < 30.0) {
     digitalWrite(LED_RED_1, HIGH);  
     digitalWrite(LED_GREEN_1, LOW);   
+    ledStatus[0] = 0;
   } else {
     digitalWrite(LED_RED_1, LOW);   
     digitalWrite(LED_GREEN_1, HIGH); 
+    ledStatus[0] = 1;
     ledCount++;
   }
 
@@ -93,9 +90,11 @@ void loop() {
   if (distance2 < 30.0) {
     digitalWrite(LED_RED_2, HIGH);  
     digitalWrite(LED_GREEN_2, LOW);   
+    ledStatus[1] = 0;
   } else {
     digitalWrite(LED_RED_2, LOW);   
     digitalWrite(LED_GREEN_2, HIGH);  
+    ledStatus[1] = 1;
     ledCount++;
   }
 
@@ -103,9 +102,11 @@ void loop() {
   if (distance3 < 30.0) {
     digitalWrite(LED_RED_3, HIGH);  
     digitalWrite(LED_GREEN_3, LOW);   
+    ledStatus[2] = 0;
   } else {
     digitalWrite(LED_RED_3, LOW);   
     digitalWrite(LED_GREEN_3, HIGH); 
+    ledStatus[2] = 1;
     ledCount++;
   }
 
@@ -113,9 +114,11 @@ void loop() {
   if (distance4 < 30.0) {
     digitalWrite(LED_RED_4, HIGH);  
     digitalWrite(LED_GREEN_4, LOW);   
+    ledStatus[3] = 0;
   } else {
     digitalWrite(LED_RED_4, LOW);   
     digitalWrite(LED_GREEN_4, HIGH); 
+    ledStatus[3] = 1;
     ledCount++;
   }
 
@@ -126,6 +129,10 @@ void loop() {
   lcd.setCursor(0, 1);
   lcd.print("Empty slots: ");
   lcd.print(ledCount);
+
+  // Send LED status to NodeMCU
+  String data = String(ledStatus[0]) + "," + String(ledStatus[1]) + "," + String(ledStatus[2]) + "," + String(ledStatus[3]);
+  Serial.println(data);
 
   delay(500);
 }
